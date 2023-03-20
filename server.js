ab50cdfe68f86d73ac217c89c408b2899b0e3bcc
Shubham Sagar <86291350+coder-sagar-sketch@users.noreply.github.com>
2023-03-20 14:45:58 +0530

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3000
+
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views') )
+app.set('view engine', 'ejs')
+
+app.listen(3000, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
