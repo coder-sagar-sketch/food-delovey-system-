ef655f8caa7bffd977bed2a4675bcc113163a213
Shubham Sagar <86291350+coder-sagar-sketch@users.noreply.github.com>
2023-03-25 20:40:30 +0530

    add to cart

---
+++ app/http/controllers/customers/cartController.js
@@ -0,0 +1,51 @@
+function cartController(){
+    return{
+        cart(req, res){
+
+            res.render('customers/cart')
+            
+        },
+        
+        update(req, res){
+           /*   let cart = {
+                items: {
+                     pizzaId: { item: pizzaObject, qty:0 },
+                
+                 },
+                 totalQty: 0,
+                 totalPrice: 0
+             } */
+// initial the cart
+            if(!req.session.cart){
+                req.session.cart = {
+                    items: {},
+                    totalQty: 0,
+                    totalPrice: 0
+                }
+               
+
+               
+
+
+            } 
+            let cart = req.session.cart
+             //cart cannot have
+             if(!cart.items[req.body._id]) {
+                cart.items[req.body._id] = {
+                    item: req.body,
+                    qty: 1
+                }
+                cart.totalQty = cart.totalQty + 1
+                cart.totalPrice = cart.totalPrice + req.body.price
+            } else {
+                cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1
+                cart.totalQty = cart.totalQty + 1
+                cart.totalPrice =  cart.totalPrice + req.body.price
+            }
+            return res.json({totalQty: req.session.cart.totalQty})
+        }
+
+    }
+}
+
+module.exports = cartController
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
@@ -5,32 +6,69 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
+const mongoose = require('mongoose')
+const session =  require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')
+
+
+
+
+//databse connection
+
+const url = 'mongodb://localhost/pizza';
+mongoose.connect( url, { useNewUrlParser: true,  useUnifiedTopology: true  })
+.then( () => console.log("connection successfull...."))
+.catch( (err) =>console.log(err));
+/* const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+})
+.catch(er => console.log( err));
+ */
+
+
 
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views') )
 app.set('view engine', 'ejs')
 
+//session store
+/* let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+}) */
 
-//Assests
+//session-config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave : false,  
+    store: MongoDbStore.create({
+        mongoUrl: url
+    }),
+    saveUninitialized: false,
+    cookie:{maxAge: 1000*60*60*24} //24 hours
+    
+}))
+
+app.use(flash())
+
+
+//Assests 
 app.use(express.static('public'))
+app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.render('home')
+
+//global middleware
+app.use((req, res, next) =>{
+    res.locals.session = req.session
+    next()
 })
 
-app.get('/cart', (req, res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login', (req, res)=>{
-    res.render('auth/login')
-})
+require('./routes/web')(app)
 
 
-app.get('/register', (req, res)=>{
-    res.render('auth/register')
-})
 
 
 
